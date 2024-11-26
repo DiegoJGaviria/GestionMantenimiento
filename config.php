5f84500fda0a8ae0a8c808374073c50e2a18b1c7
DiegoJGaviria <145846999+DiegoJGaviria@users.noreply.github.com>
2024-11-26 15:55:03 -0500

    ss

--- config.php
+++ config.php
@@ -1,2 +1,22 @@
-...moved to src/config.php...
+<?php
+session_start();
+
+$host = "localhost";
+$username = "diego@gmail.com";
+$password = "123456";
+$dbname = "tu_base_de_datos";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+function checkLogin() {
+    if (!isset($_SESSION['user_id'])) {
+        header("Location: login.php");
+        exit();
+    }
+}
+?>
 
